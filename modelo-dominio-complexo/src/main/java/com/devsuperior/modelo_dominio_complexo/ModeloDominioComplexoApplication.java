984deb9101c1b2a070cb8e28f792aa9e2d49558b
MiltonRafael <miltonrafael166@gmail.com>
2026-03-30 20:31:02 -0300

    first commit

---
+++ modelo-dominio-complexo/src/main/java/com/devsuperior/modelo_dominio_complexo/ModeloDominioComplexoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modelo_dominio_complexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloDominioComplexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloDominioComplexoApplication.class, args);
+	}
+
+}
---
+++ modelo-dominio-complexo/src/test/java/com/devsuperior/modelo_dominio_complexo/ModeloDominioComplexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modelo_dominio_complexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloDominioComplexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
