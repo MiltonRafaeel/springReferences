f17e9f7a40e17f89ecb41769b53239f92655441d
MiltonRafael <miltonrafael166@gmail.com>
2026-03-31 20:00:42 -0300

    course, notifcation and offer

--- modelo-dominio-complexo/src/main/java/com/devsuperior/modelo_dominio_complexo/entities/User.java
+++ modelo-dominio-complexo/src/main/java/com/devsuperior/modelo_dominio_complexo/entities/User.java
@@ -1,7 +1,9 @@
 package com.devsuperior.modelo_dominio_complexo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -13,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +35,9 @@ public class User implements Serializable {
 			joinColumns = @JoinColumn(name = "user_id"),
 			inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+	
+	@OneToMany(mappedBy = "user")
+	List<Notification> notifications = new ArrayList<>();
 	
 	public User() {
 		
@@ -78,6 +84,10 @@ public class User implements Serializable {
 
 	public Set<Role> getRoles() {
 		return roles;
+	}
+
+	public List<Notification> getNotifications() {
+		return notifications;
 	}
 
 	@Override
--- modelo-dominio-complexo/src/main/resources/import.sql
+++ modelo-dominio-complexo/src/main/resources/import.sql
@@ -13,4 +13,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://images.pexels.com/photos/7647950/pexels-photo-7647950.jpeg', 'https://www.pexels.com/pt-br/foto/plantar-planta-folhas-fechar-se-7294541/');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07Z', TIMESTAMP WITH TIME ZONE '2021-07-13T20:50:07Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07Z', TIMESTAMP WITH TIME ZONE '2022-07-13T20:50:07Z', 1);
 
