97e1cfe1053edd72d59e5428f83b9537eb81a192
MiltonRafael <miltonrafael166@gmail.com>
2026-04-01 06:12:00 -0300

    resource

---
+++ modelo-dominio-complexo/src/main/java/com/devsuperior/modelo_dominio_complexo/enums/ResourceType.java
@@ -0,0 +1,7 @@
+package com.devsuperior.modelo_dominio_complexo.enums;
+
+public enum ResourceType {
+	
+	LESSON_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+
+}
--- modelo-dominio-complexo/src/main/resources/import.sql
+++ modelo-dominio-complexo/src/main/resources/import.sql
@@ -18,3 +18,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07Z', TIMESTAMP WITH TIME ZONE '2021-07-13T20:50:07Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07Z', TIMESTAMP WITH TIME ZONE '2022-07-13T20:50:07Z', 1);
 
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'trilha principal do curso', 1, 'https://images.pexels.com/photos/7647950/pexels-photo-7647950.jpeg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'tire suas duvidas', 2, 'https://images.pexels.com/photos/7647950/pexels-photo-7647950.jpeg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas', 3, 'https://images.pexels.com/photos/7647950/pexels-photo-7647950.jpeg', 0, 1);
+
