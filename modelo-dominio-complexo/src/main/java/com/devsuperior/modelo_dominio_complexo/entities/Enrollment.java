2fcd96d916ea4d8120c02ef79e488e52d7f2b2ee
MiltonRafael <miltonrafael166@gmail.com>
2026-04-01 16:42:34 -0300

    lesson seed, fix: lessonsDone

--- modelo-dominio-complexo/src/main/java/com/devsuperior/modelo_dominio_complexo/entities/Enrollment.java
+++ modelo-dominio-complexo/src/main/java/com/devsuperior/modelo_dominio_complexo/entities/Enrollment.java
@@ -2,12 +2,15 @@ package com.devsuperior.modelo_dominio_complexo.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.devsuperior.modelo_dominio_complexo.pk.EnrollmentPK;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +28,9 @@ public class Enrollment implements Serializable {
 	private Instant refundMoment;
 	private boolean available;
 	private boolean onlyUpdate;
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
 	
 	public Enrollment() {
 		
--- modelo-dominio-complexo/src/main/resources/import.sql
+++ modelo-dominio-complexo/src/main/resources/import.sql
@@ -28,3 +28,18 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07Z', null, true, false);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do capitulo 1: quebrando regras', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'material de apoio', 'https://www.youtube.com/watch?v=xC_yKw3MYX4&t=3805s');
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 2 do capitulo 1: superando limites', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, 'material de apoio', 'https://www.youtube.com/watch?v=xC_yKw3MYX4&t=3805s');
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 3 do capitulo 1: quebrando barreiras', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, 'material de apoio', 'https://www.youtube.com/watch?v=xC_yKw3MYX4&t=3805s');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa do capitulo 1: quebrando barreiras', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, aprove_Count, weight, due_Date) VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-07-25T20:50:07Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
+
