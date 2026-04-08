dabe8127b1bdc666f56b157edddcb80561bf2959
MiltonRafael <miltonrafael166@gmail.com>
2026-04-08 09:24:20 -0300

    Post new user

--- workshop-webflux-mongo/src/main/java/com/example/demo/services/UserService.java
+++ workshop-webflux-mongo/src/main/java/com/example/demo/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dtos.UserDTO;
+import com.example.demo.models.entities.User;
 import com.example.demo.repositories.UserRepository;
 import com.example.demo.services.exceptions.ResourceNotFoundException;
 
@@ -24,5 +25,18 @@ public class UserService {
 		return repository.findById(id)
 				.map(existingUser -> new UserDTO(existingUser))
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Not Found")));
+	}
+	
+	public Mono<UserDTO> insert(UserDTO dto) {
+		User entity = new User();
+		copyDtotoEntity(dto, entity);
+		Mono<UserDTO> result = repository.save(entity).map(user -> new UserDTO(user));
+		return result;
+	}
+
+	private void copyDtotoEntity(UserDTO dto, User entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		
 	}
 }
