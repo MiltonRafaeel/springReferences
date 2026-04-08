9b6bdd042fdd5256524d6bf0d54d9e1e72c5c03f
MiltonRafael <miltonrafael166@gmail.com>
2026-04-08 08:49:09 -0300

    Database seeding

--- workshop-webflux-mongo/src/main/java/com/example/demo/config/TestConfig.java
+++ workshop-webflux-mongo/src/main/java/com/example/demo/config/TestConfig.java
@@ -15,6 +15,8 @@ import com.example.demo.repositories.PostRepository;
 import com.example.demo.repositories.UserRepository;
 
 import jakarta.annotation.PostConstruct;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @Profile("test")
@@ -29,6 +31,34 @@ public class TestConfig {
 	@PostConstruct
 	public void init() {
 		
+		Mono<Void> deleteUsers = userRepository.deleteAll();
+		deleteUsers.subscribe();
+		
+		Mono<Void> deletePosts = postRepository.deleteAll();
+		deletePosts.subscribe();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Flux<User> insertUsers = userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		insertUsers.subscribe();
+		
+		Post post1 = new Post(null, Instant.parse("2021-02-13T11:15:01Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new Author(maria));
+		Post post2 = new Post(null, Instant.parse("2021-02-14T10:05:49Z"), "Bom dia", "Acordei feliz hoje!", new Author(maria));
+
+		Comment c1 = new Comment("Boa viagem mano!", Instant.parse("2021-02-13T14:30:01Z"), new Author(alex));
+		Comment c2 = new Comment("Aproveite", Instant.parse("2021-02-13T15:38:05Z"), new Author(bob));
+		Comment c3 = new Comment("Tenha um ótimo dia!", Instant.parse("2021-02-14T12:34:26Z"), new Author(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		Flux<Post> insertPosts = postRepository.saveAll(Arrays.asList(post1, post2));
+		insertPosts.subscribe();
+	
+		
+		/*
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 
@@ -52,5 +82,6 @@ public class TestConfig {
 
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);	
-	}
+		*/
+	}	
 }
